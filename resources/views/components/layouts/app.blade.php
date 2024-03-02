7b72a2ff5707aa38e475f5ec36bb446cbee85df6
ivanpakpahan27 <ivanpakpahangn@gmail.com>
2024-03-02 19:09:45 +0700

    .

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en" class="h-100" data-bs-theme="auto">
+
+<head>
+  <script src="{{ asset('assets/bootstrap/assets/js/color-modes.js') }}"></script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="generator" content="Hugo 0.122.0">
+  <title>GSJA Pemuridan</title>
+  <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo.png') }}" type="image/x-icon">
+
+  <link href="{{ asset('assets/bootstrap/assets/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('assets/css/cover.css') }}" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Font Themes -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Nav -->
+  <nav class="navbar navbar-expand-lg bg-body-secondary border-bottom">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">
+        <img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo" width="45" height="40"
+          class="d-inline-block align-text-top"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Galeri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Alkitab</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Lainnya</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Nav -->
+
+  <!-- Main Content -->
+  <div class="main-content container-fluid pt-5 pb-5 bg-body">
+    {{ $slot }}
+  </div>
+  <!-- Main Content -->
+
+  <!-- Footer -->
+  <div class="p-3 bg-body-secondary border">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <svg class="bi" width="30" height="24">
+            <use xlink:href="#bootstrap" />
+          </svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2024 GSJA Pemuridan</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-body-secondary" href="#"><i
+              class="fa-xl fa-brands fa-instagram"></i></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-xl fa-brands fa-facebook"></i></a>
+        </li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-xl fa-brands fa-youtube"></i></a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+  <!-- Footer -->
+
+  <!-- Themes Setting -->
+  <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+    <symbol id="check2" viewBox="0 0 16 16">
+      <path
+        d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+    </symbol>
+    <symbol id="circle-half" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
+    </symbol>
+    <symbol id="moon-stars-fill" viewBox="0 0 16 16">
+      <path
+        d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+      <path
+        d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
+    </symbol>
+    <symbol id="sun-fill" viewBox="0 0 16 16">
+      <path
+        d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+    </symbol>
+  </svg>
+  <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+    <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button"
+      aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
+      <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
+        <use href="#circle-half"></use>
+      </svg>
+      <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
+    </button>
+    <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light"
+          aria-pressed="false">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#sun-fill"></use>
+          </svg>
+          Mode Terang
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
+          aria-pressed="false">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#moon-stars-fill"></use>
+          </svg>
+          Mode Gelap
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto"
+          aria-pressed="true">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#circle-half"></use>
+          </svg>
+          Auto
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </div>
+  <script src="{{ asset('assets/bootstrap/assets/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- Themes Setting -->
+</body>
+
+</html>
---
+++ resources/views/livewire/beranda.blade.php
@@ -0,0 +1,205 @@
+<div>
+  <div class="motto">
+    <div class="text-center">
+      <h3>
+        Cinta Tuhan, Rendah hati, Jujur dan Rajin.
+      </h3>
+    </div>
+  </div>
+  <hr>
+  <div class="carousel">
+    <div>
+      <div id="carouselExampleFade" class="carousel slide carousel-fade p-3 ">
+        <div class="carousel-inner rounded">
+          <div class="carousel-item active">
+            <img src="{{ asset('assets/images/carousels/1.png') }}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assets/images/carousels/2.png') }}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assets/images/carousels/3.png') }}" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="history">
+    <div class="text-center">
+      <h5>
+        Sejarah Gereja
+      </h5>
+    </div>
+    <div class="shadow p-5 mb-5 bg-body-tertiary rounded">
+      Penginjilan pertama dirintis oleh Pendeta Agus Manuhutu (&#10014;) yang berasal dari Palembang.
+      Pelayanan injil dilaksanakan di Unit 1 pada tahun 1986, tepatnya di kediaman
+      keluarga besar Bapak Gideon. Selanjutnya, pelayanan mulai diadakan di Gereja Unit 5
+      pada tahun 1987, yang digembalai oleh Pendeta Hosea (&#10014;) selama 5 tahun, yaitu pada tahun
+      1987 hingga tahun 1992. Pelayanan kemudian dilanjutkan oleh Pendeta Thomas
+      Ramlan selama 8 tahun, yakni pada tahun 1992 hingga tahun 2000. Selanjutnya,
+      pelayanan dipimpin oleh Pendeta Donald sejak tahun 2000 hingga tahun 2002.
+      Kemudian, Pendeta Simon Mukadi (&#10014;) melakukan pelayanan selama 8 tahun,
+      pada tahun 2002 sampai 2010. Dilanjutkan oleh Pendeta Ramli Nababan selama 4
+      tahun, yaitu tahun 2010-2014, dan Pendeta Matius Junianto pada tahun 2015 -
+      hingga saat ini. Catatan: Pdt. Simon Mukadi selalu mendampingi gembala sepanjang
+      pelayanan Gereja dan difungsikan sebagai tenaga pelayan Injil GSJA
+      Pemuridan.
+      <br><br>
+      Selama berdiri, Gereja telah berganti nama sebanyak 2 kali. Nama
+      yang pertama digunakan dari tahun 1987-2015, yaitu GSJA "Imanuel". Kemudian
+      diubah menjadi GSJA "Pemuridan" dari tahun 2015 - sampai sekarang. Gereja
+      telah melakukan kegiatan pembangunan sebanyak dua (2) kali yaitu pada tahun
+      1987 dan juga tahun 2008.
+    </div>
+  </div>
+  <hr>
+  <div class="organization">
+    <div class="text-center">
+      <h5>
+        Bagan Organisasi Gereja
+      </h5>
+    </div>
+    <div class="shadow p-5 mb-5 bg-body-tertiary rounded">
+      <img src="{{ asset('assets/images/organization/Diagram GSJA_.png') }}" class="rounded d-block w-100"
+        alt="">
+    </div>
+  </div>
+  <div class="activity" style="overflow: auto">
+    <div class="text-center">
+      <h5>
+        Jadwal kegiatan
+      </h5>
+    </div>
+    <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
+      <table class="table">
+        <thead class="table-primary">
+          <tr>
+            <th>No</th>
+            <th>Nama Kegiatan</th>
+            <th>Hari</th>
+            <th>Jam</th>
+            <th>Tempat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ibadah Raya</td>
+            <td>Minggu</td>
+            <td>10:00 WIB</td>
+            <td>Gereja (Ds. Tanah Tinggi)</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Ibadah KKA Unit 10</td>
+            <td>Kamis</td>
+            <td>18:30 WIB</td>
+            <td>Rumah Jema'at Unit 10</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Doa Puasa</td>
+            <td>Jum'at</td>
+            <td>18:00 WIB</td>
+            <td>Rumah Jema'at</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Ibadah PT Sandabi</td>
+            <td>Minggu</td>
+            <td>- WIB</td>
+            <td>Mesh Sandabi</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Ibadah Tanah Hitam</td>
+            <td>Minggu</td>
+            <td>- WIB</td>
+            <td>Rumah Jema'at Tanah Hitam</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Ibadah Rumah Tangga</td>
+            <td>Selasa</td>
+            <td>- WIB</td>
+            <td>Rumah Jema'at</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="pengakuan-iman">
+    <div class="text-center">
+      <h5>
+        Pengakuan Iman Gereja
+      </h5>
+    </div>
+    <div class="shadow p-5 mb-5 bg-body-tertiary rounded">
+      Kami percaya bahwa:
+      Alkitab adalah Firman Allah yang diilhamkan tanpa salah,
+      satu-satunya kaidah yang mutlak
+      dan berwewenang bagi iman dan perilaku manusia.
+      Allah adalah Esa, hadir secara kekal dalam tiga Oknum: Allah Bapa, Allah Putra, dan Allah Roh Kudus.
+      <br><br>
+      Tuhan Yesus Kristus:
+      Ilahi adanya, lahir dari seorang anak dara, hidup tanpa dosa,
+      melakukan mujizat-mujizat, menebus manusia yang berdosa
+      melalui kematianNya, bangkit secara badani,
+      naik ke surga dan dimuliakan di sebelah kanan Allah Bapa,
+      akan datang kembali ke bumi dalam kuasa dan kemuliaan untuk memerintah dalam Kerajaan Seribu Tahun.
+      <br><br>
+      Oleh pelanggaran satu orang (Adam) dosa telah masuk ke dalam dunia dan maut oleh sebab dosa;
+      demikianlah maut telah menimpa semua orang karena semua orang telah berbuat dosa.
+      Keampunan dan penyucian dari dosa hanyalah melalui pertobatan dan iman kepada kuasa penyucian darah Kristus.
+      <br><br>
+      Pembaharuan yang dikerjakan oleh Roh Kudus mutlak perlu bagi keselamatan seseorang.
+      Karya penebusan Kristus di atas salib menyediakan kesembuhan bagi tubuh manusia melalui doa dan iman.
+      Pengudusan dikerjakan oleh Roh Kudus yang mendiami orang percaya.
+      Baptisan Roh Kudus menurut Kisah Para Rasul 2:4
+      dikaruniakan kepada orang beriman yang memohon dari Allah.
+      <br><br>
+      Pengangkatan Gereja pada waktu kedatangan Kristus di awan-awan merupakan pengharapan yang bahagia bagi
+      orang-orang
+      percaya.
+      Kebangkitan tubuh bagi orang-orang percaya maupun yang tidak percaya;
+      yang pertama untuk menerima hidup yang kekal
+      dan yang kedua untuk menerima hukuman yang kekal. Amin
+      <br><br>
+      TUHAN YESUS MEMBERKATI
+    </div>
+  </div>
+  <div class="pengakuan-iman-rasuli">
+    <div class="text-center">
+      <h5>
+        Pengakuan Iman Rasuli
+      </h5>
+    </div>
+    <div class="shadow p-5 mb-5 bg-body-tertiary rounded">
+      Aku percaya kepada Allah Bapa Yang Mahakuasa, Khalik langit dan bumi dan
+      kepada Yesus Kristus AnakNya yang tunggal Tuhan kita;
+      <br /><br />
+      Yang dikandung oleh Roh Kudus lahir dari anak dara Maria, yang menderita
+      sengsara di bawah pemerintahan Pontius Pilatus disalibkan, mati, dan
+      dikuburkan, turun ke dalam kerajaan maut
+      <br /><br />
+      pada hari yang ketiga, bangkit pula dari antara orang mati; naik ke surga,
+      duduk di sebelah kanan Allah Bapa Yang Maha Kuasa; dari sana Ia akan
+      datang untuk menghakimi orang yang hidup dan yang mati.
+      <br /><br />
+      Aku percaya kepada Roh Kudus, Gereja yang kudus dan am, persekutuan
+      orang-orang kudus, pengampunan dosa, kebangkitan daging dan hidup yang
+      kekal. Amin
+      <br /><br />
+      TUHAN YESUS MEMBERKATI
+    </div>
+  </div>
+</div>
